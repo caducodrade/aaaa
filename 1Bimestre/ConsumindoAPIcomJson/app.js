8bf329cfd4d2d183ffc2d388f888faf55a6d68ef
Hericson Ramos <hericson@bwfti.com.br>
2020-03-16 20:40:28 -0300

    Aula 12 Mar

--- 1Bimestre/ConsumindoAPIcomJson/app.js
+++ 1Bimestre/ConsumindoAPIcomJson/app.js
@@ -3,7 +3,6 @@
  * npm install request*/
 
 var request = require('request');
-
 /**
  *  Para acessar o webservice, um CEP no formato de {8} dígitos deve ser fornecido, por exemplo: "01001000".
 Após o CEP, deve ser fornecido o tipo de retorno desejado, que deve ser "json", "xml", "piped" ou "querty".
@@ -11,7 +10,7 @@ Exemplo de pesquisa por CEP:
 viacep.com.br/ws/01001000/json/ 
 */
 //informe o seu cep aqui
-var cep = '86090020';
+var cep = '86183877';
 
 request('http://viacep.com.br/ws/'+ cep +'/json/', function (error, response, body) {
   console.log('error:', error); // Print o erro que ocorreu
@@ -24,6 +23,7 @@ request('http://viacep.com.br/ws/'+ cep +'/json/', function (error, response, bo
   poderemos acessar os atributos do objeto normalmente. */
   var json = JSON.parse(body)
     cidade = json.localidade;
+    
     request('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'&APPID=604b9d1c6b9e0b3e94b0b3ffd01fe6ba', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
