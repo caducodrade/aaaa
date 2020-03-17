805dd90aba159397aa0cdfd51d330671a8c846e7
Hericson <hericson@bwfti.com.br>
2020-03-17 01:32:13 -0300

    Exercício Craindo server  + Json

--- 1Bimestre/ServerJs/app.js
+++ 1Bimestre/ServerJs/app.js
@@ -1,5 +1,6 @@
 //Busca a  bibioteca http
 const http = require('http');
+var request = require('request');
 
 
 //seta o ip ou server que sera execultado
@@ -10,11 +11,24 @@ const port = 8080;
 
 //cria uma constante que gurdara o server
 const server = http.createServer((req, res) => {
-    
+    //http://127.0.0.1:8080/
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Faccar, teste!\n');
 
+    //informe o seu cep aqui
+var cep = '86183877';
+
+request('http://viacep.com.br/ws/'+ cep +'/json/', function (error, response, body) {
+  console.log('error:', error); // Print o erro que ocorreu
+  console.log('statusCode:', response && response.statusCode); // Imprima o código de status da resposta se uma resposta foi recebida
+  console.log('body:', JSON.parse(body)); // imprime o Json recebido
+  var json = JSON.parse(body)
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('CEP: '+ json.cep+ 
+    '\n Rua: '  +  json.logradouro +
+    '\n cidade:'  +  json.localidade +'  '+json.uf+
+    '\n');
+  });
 });
 
 server.listen(port, hostname, () => {
